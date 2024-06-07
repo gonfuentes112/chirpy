4fe3d0f8aa760cab21638976ee03950526de2052
gekishou <gonfuentes112@gmail.com>
2024-06-06 22:13:41 -0300

    ch4

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	fmt.Println("Starting server at :8080")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("Server failed:", err)
+	}
+}
