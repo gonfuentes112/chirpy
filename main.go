d4c54377a69e9ceb5f9e6c976fe771fee03a3fc2
gekishou <gonfuentes112@gmail.com>
2024-06-06 22:36:13 -0300

    ch5

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Println("Starting server at :8080")
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Server failed:", err)
 	}
 }
-
-//a
