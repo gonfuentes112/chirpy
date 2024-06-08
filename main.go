a8d4b71a675fdc7f7230f84c0887002f69240197
gekishou <gonfuentes112@gmail.com>
2024-06-07 22:33:20 -0300

    routingch1

--- main.go
+++ main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits int
+}
 
-	server := &http.Server{
-		Addr:    ":8080",
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	fmt.Println("Starting server at :8080")
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Println("Server failed:", err)
-	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
